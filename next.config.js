9d950528d712d3aef25d7f917cbfae473f70f1c5
jeylanicodes <83839350+jeylanicodes@users.noreply.github.com>
2022-07-03 22:12:56 +0100

    fix: moved to environment variable

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
-  env: {
-    DVLA_API_KEY: 'sZpLFE5WCU3w0LYl81rQR4kyx7q0w7ZI1KeMJaGp'
-  }
+  swcMinify: true
 };
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,9 @@
 type Config = {
   dvlaApiKey: string;
+  mailerSendApiKey: string;
 };
 
 export const config: Config = {
-  dvlaApiKey: process.env.DVLA_API_KEY ?? ''
+  dvlaApiKey: process.env.DVLA_API_KEY ?? '',
+  mailerSendApiKey: process.env.MAILER_SEND_API_KEY ?? ''
 };
