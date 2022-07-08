1f8312d41fa256b6d85df38e574f9fab4ed81256
shoqi1122 <alamshokat@gmail.com>
2022-07-08 17:03:01 +0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>Zemoto</title>
+        <title>Zemoto - test</title>
         <meta name='description' content='Zemoto, the best way to sell your bike' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
