edcbd397a7e4202ea8afe1a1f25882f0b98ac26d
jeylanicodes <83839350+jeylanicodes@users.noreply.github.com>
2022-07-28 13:50:19 -0400

    feat: add cookie banner

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,11 @@
 type Config = {
   dvlaApiKey: string;
   mailerSendApiKey: string;
+  gaTrackingId: string;
 };
 
 export const config: Config = {
   dvlaApiKey: process.env.DVLA_API_KEY ?? '',
-  mailerSendApiKey: process.env.MAILER_SEND_API_KEY ?? ''
+  mailerSendApiKey: process.env.MAILER_SEND_API_KEY ?? '',
+  gaTrackingId: process.env.NEXT_PUBLIC_GA_TRACKING_ID ?? ''
 };
